1cd108bbe329a1e62a08c60909e979033bd35dac
Antonio Jack <ict2jack@gmail.com>
2021-02-14 22:29:39 +0700

    🚧  Working on it...

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,6 +27,46 @@ const Home = () => (
         </div>
       </nav>
     </header>
+    <div className="content">
+      <nav className="sidebar">
+        <ul className="side-nav">
+          <li className="side-nav__item side-nav__item--active">
+            <a href="#" className="side-nav__link">
+              <svg className="side-nav__icon">
+                <img src="/img/sprite.svg#icon-home"></img>
+              </svg>
+              <span>Hotel</span>
+            </a>
+          </li>
+          <li className="side-nav__item">
+            <a href="#" className="side-nav__link">
+              <svg className="side-nav__icon">
+                <img src="/img/sprite.svg#icon-aircraft-take-off"></img>
+              </svg>
+              <span>Flight</span>
+            </a>
+          </li>
+          <li className="side-nav__item">
+            <a href="#" className="side-nav__link">
+              <svg className="side-nav__icon">
+                <img src="/img/sprite.svg#icon-key"></img>
+              </svg>
+              <span>Car rental</span>
+            </a>
+          </li>
+          <li className="side-nav__item">
+            <a href="#" className="side-nav__link">
+              <svg className="side-nav__icon">
+                <img src="img/sprite.svg#icon-map"></img>
+              </svg>
+              <span>Tours</span>
+            </a>
+          </li>
+        </ul>
+
+        <div className="legal">&copy; 2017 by trillo. All rights reserved.</div>
+      </nav>
+    </div>
   </div>
 )
 
