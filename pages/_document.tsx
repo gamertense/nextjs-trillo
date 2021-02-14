1f0c77695627e1955fbc102a465f46f4fe744b78
Antonio Jack <ict2jack@gmail.com>
2021-02-14 21:59:39 +0700

    💄 layout

---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600"
+            rel="stylesheet"
+          />
+          <link rel="stylesheet" href="css/style.css" />
+          <link rel="shortcut icon" type="image/png" href="img/favicon.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
